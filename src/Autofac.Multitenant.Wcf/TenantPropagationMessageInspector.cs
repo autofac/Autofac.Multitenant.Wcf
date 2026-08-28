cb8c318af41740a72f9f5d757dd5837fe095f1cc
Travis Illig <tillig@paraesthesia.com>
2026-08-28 14:36:33 -0700

    Remove unused usings.

--- src/Autofac.Multitenant.Wcf/TenantPropagationMessageInspector.cs
+++ src/Autofac.Multitenant.Wcf/TenantPropagationMessageInspector.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Autofac Project. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System;
-using System.Diagnostics.CodeAnalysis;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Dispatcher;
